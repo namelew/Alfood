38912ed0ed520990f7ed32dfc4f260b0a8faf52f
namelew <diogomaciel.cunha@gmail.com>
2023-08-24 09:52:08 -0300

    Criando botão de ver mais

--- src/components/RestaurantsList/index.tsx
+++ src/components/RestaurantsList/index.tsx
@@ -3,23 +3,35 @@ import style from './RestaurantsList.module.scss';
 import Restaurant from './Restaurant';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { IPager } from 'interfaces/IPager';
 
 const RestaurantsList = () => {
 
     const [restaurants, setRestaurants] = useState<IRestaurant[]>([]);
+    const [nextPage, setNextPage] = useState('');
 
     useEffect(() => {
-        axios.get('http://localhost:8000/api/v1/restaurantes/').then( (response) => {
+        axios.get<IPager<IRestaurant>>('http://localhost:8000/api/v1/restaurantes/').then( (response) => {
             setRestaurants(response.data.results);
-            console.log(restaurants);
+            setNextPage(response.data.next);
         }).catch( (error) => {
             console.log(error);
         });
     }, []);
 
+    const seeMore = () => {
+        axios.get<IPager<IRestaurant>>(nextPage).then( (response) => {
+            setRestaurants([...restaurants,...response.data.results]);
+            setNextPage(response.data.next);
+        }).catch( (error) => {
+            console.log(error);
+        });
+    };
+
     return (<section className={style.RestaurantsList}>
         <h1>Os restaurants mais <em>bacanas</em>!</h1>
         {restaurants?.map(item => <Restaurant restaurant={item} key={item.id} />)}
+        {nextPage && <button onClick={seeMore}>ver mais</button>}
     </section>);
 };
 
